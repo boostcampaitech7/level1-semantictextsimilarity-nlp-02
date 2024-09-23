0b9c6d79b6940642e593fd65f7e0a718906089bf
kangjeonhwi <kangjeonhwi@gmail.com>
2024-09-23 17:24:56 +0900

    Revert "utils에 LLAMA promt 이용 data augmentation 하는 코드 추가, data에 augmented 디렉토리 추가가"
    This reverts commit 1c5b582562c0552ce7407d333e177d1f0367b944.

--- test.py
+++ test.py
@@ -1,18 +1,14 @@
 from utils.util import load_config
 from data_loaders.lightning_loader import Dataloader
-from models.lightning_model_L2loss import Model
+from models.lightning_model import Model
 
 import pytorch_lightning as pl
 import torch
 import pandas as pd
 
-import os
-
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
 if __name__ == '__main__':
     # 하이퍼 파라미터 등 각종 설정값을 입력받습니다
-    config = load_config('configs/deberta_large_120.yaml')
+    config = load_config('configs/deberta_base_80.yaml')
 
     # dataloader와 model을 생성합니다.
     dataloader = Dataloader(config['model_name'], config['batch_size'], config['shuffle'], config['train_path'], 
@@ -23,8 +19,8 @@ if __name__ == '__main__':
 
     # Inference part
     # 저장된 모델로 예측을 진행합니다.
-    model = torch.load('saves/'+config['model_nick']+'-L2.pt')
-    checkpoint = torch.load('saves/models/deberta_large_120-L2-epoch=11-val_pearson=0.939-val_loss=0.264.ckpt')
+    model = torch.load('saves/'+config['model_nick'])
+    checkpoint = torch.load('saves/models/deberta-large-epoch=07-val_pearson=0.937-max120.ckpt')
     model.load_state_dict(checkpoint['state_dict'])
     predictions = trainer.predict(model=model, datamodule=dataloader)
 
@@ -34,4 +30,4 @@ if __name__ == '__main__':
     # output 형식을 불러와서 예측된 결과로 바꿔주고, output.csv로 출력합니다.
     output = pd.read_csv('./data/sample_submission.csv')
     output['target'] = predictions
-    output.to_csv(config['model_nick']+'_L2-output.csv', index=False)
+    output.to_csv('output.csv', index=False)
