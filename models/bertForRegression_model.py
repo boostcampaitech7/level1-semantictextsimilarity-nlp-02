aa8a2554b2b616a532b74dd4b8fa3e11e9b89d71
pica-git0 <jilp1598.git@gmail.com>
2024-09-26 00:44:32 +0900

    feat: models DeBERTa 모델 클래스 구현

--- models/bertForRegression_model.py
+++ models/bertForRegression_model.py
@@ -1,5 +1,6 @@
-from transformers import BertModel, BertPreTrainedModel
+from transformers import BertModel, BertPreTrainedModel, DebertaV2Model, DebertaV2PreTrainedModel
 import torch.nn as nn
+import torchmetrics
 
 
 class BertForRegression(BertPreTrainedModel):
@@ -12,5 +13,19 @@ class BertForRegression(BertPreTrainedModel):
         outputs = self.bert(
             input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
         cls_output = outputs[1]  # [CLS] 토큰의 임베딩
+        regression_output = self.regressor(cls_output)  # 단일 값 예측
+        return regression_output
+
+
+class DeBERTaForRegression(DebertaV2PreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.bert = DebertaV2Model(config)
+        self.regressor = nn.Linear(config.hidden_size, 1)  # 단일 실수 값을 출력하도록 설정
+
+    def forward(self, input_ids, attention_mask=None, token_type_ids=None):
+        outputs = self.bert(
+            input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+        cls_output = outputs.last_hidden_state[:, 0, :]  # [CLS] 토큰의 임베딩
         regression_output = self.regressor(cls_output)  # 단일 값 예측
         return regression_output
