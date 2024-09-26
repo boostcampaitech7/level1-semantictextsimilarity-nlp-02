78590a50444740f6a7fc9c042a2bcc5888d305b0
pica-git0 <jilp1598.git@gmail.com>
2024-09-26 13:16:10 +0900

    feat: roberta 모델 추가

--- models/bertForRegression_model.py
+++ models/bertForRegression_model.py
@@ -1,4 +1,4 @@
-from transformers import BertModel, BertPreTrainedModel, DebertaV2Model, DebertaV2PreTrainedModel
+from transformers import BertModel, BertPreTrainedModel, DebertaV2Model, DebertaV2PreTrainedModel, RobertaModel, RobertaPreTrainedModel
 import torch.nn as nn
 import torchmetrics
 
@@ -23,9 +23,24 @@ class DeBERTaForRegression(DebertaV2PreTrainedModel):
         self.bert = DebertaV2Model(config)
         self.regressor = nn.Linear(config.hidden_size, 1)  # 단일 실수 값을 출력하도록 설정
 
-    def forward(self, input_ids, attention_mask=None, token_type_ids=None):
+    def forward(self, input_ids, attention_mask=None):
         outputs = self.bert(
-            input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+            input_ids=input_ids, attention_mask=attention_mask)
         cls_output = outputs.last_hidden_state[:, 0, :]  # [CLS] 토큰의 임베딩
+        regression_output = self.regressor(cls_output)  # 단일 값 예측
+        return regression_output
+
+
+class RoBERTaForRegression(RobertaPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.roberta = RobertaModel(config)
+        self.regressor = nn.Linear(config.hidden_size, 1)  # 단일 실수 값을 출력하도록 설정
+
+    def forward(self, input_ids, attention_mask=None):
+        outputs = self.roberta(
+            input_ids=input_ids, attention_mask=attention_mask)
+        cls_output = outputs.pooler_output if outputs.pooler_output is not None else outputs.last_hidden_state[
+            :, 0, :]
         regression_output = self.regressor(cls_output)  # 단일 값 예측
         return regression_output
